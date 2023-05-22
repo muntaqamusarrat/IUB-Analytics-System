0fb971f443ab2d685fb2f7cf66370067bd31bee9
muntaqamusarrat <134136312+muntaqamusarrat@users.noreply.github.com>
2023-05-22 13:31:25 +0600

    Add files via upload

--- operators.js
+++ operators.js
@@ -1,56 +1,94 @@
+"use strict";
 var __defProp = Object.defineProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
-var __export = (target, all) => {
-  __markAsModule(target);
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
 };
-__export(exports, {
-  Op: () => Op,
-  default: () => operators_default
-});
-const Op = {
-  eq: Symbol.for("eq"),
-  ne: Symbol.for("ne"),
-  gte: Symbol.for("gte"),
-  gt: Symbol.for("gt"),
-  lte: Symbol.for("lte"),
-  lt: Symbol.for("lt"),
-  not: Symbol.for("not"),
-  is: Symbol.for("is"),
-  in: Symbol.for("in"),
-  notIn: Symbol.for("notIn"),
-  like: Symbol.for("like"),
-  notLike: Symbol.for("notLike"),
-  iLike: Symbol.for("iLike"),
-  notILike: Symbol.for("notILike"),
-  startsWith: Symbol.for("startsWith"),
-  endsWith: Symbol.for("endsWith"),
-  substring: Symbol.for("substring"),
-  regexp: Symbol.for("regexp"),
-  notRegexp: Symbol.for("notRegexp"),
-  iRegexp: Symbol.for("iRegexp"),
-  notIRegexp: Symbol.for("notIRegexp"),
-  between: Symbol.for("between"),
-  notBetween: Symbol.for("notBetween"),
-  overlap: Symbol.for("overlap"),
-  contains: Symbol.for("contains"),
-  contained: Symbol.for("contained"),
-  adjacent: Symbol.for("adjacent"),
-  strictLeft: Symbol.for("strictLeft"),
-  strictRight: Symbol.for("strictRight"),
-  noExtendRight: Symbol.for("noExtendRight"),
-  noExtendLeft: Symbol.for("noExtendLeft"),
-  and: Symbol.for("and"),
-  or: Symbol.for("or"),
-  any: Symbol.for("any"),
-  all: Symbol.for("all"),
-  values: Symbol.for("values"),
-  col: Symbol.for("col"),
-  placeholder: Symbol.for("placeholder"),
-  join: Symbol.for("join"),
-  match: Symbol.for("match")
+const _ = require("lodash");
+const Op = require("../../../operators");
+const Utils = require("../../../utils");
+const OperatorHelpers = {
+  OperatorMap: {
+    [Op.eq]: "=",
+    [Op.ne]: "!=",
+    [Op.gte]: ">=",
+    [Op.gt]: ">",
+    [Op.lte]: "<=",
+    [Op.lt]: "<",
+    [Op.not]: "IS NOT",
+    [Op.is]: "IS",
+    [Op.in]: "IN",
+    [Op.notIn]: "NOT IN",
+    [Op.like]: "LIKE",
+    [Op.notLike]: "NOT LIKE",
+    [Op.iLike]: "ILIKE",
+    [Op.notILike]: "NOT ILIKE",
+    [Op.startsWith]: "LIKE",
+    [Op.endsWith]: "LIKE",
+    [Op.substring]: "LIKE",
+    [Op.regexp]: "~",
+    [Op.notRegexp]: "!~",
+    [Op.iRegexp]: "~*",
+    [Op.notIRegexp]: "!~*",
+    [Op.between]: "BETWEEN",
+    [Op.notBetween]: "NOT BETWEEN",
+    [Op.overlap]: "&&",
+    [Op.contains]: "@>",
+    [Op.contained]: "<@",
+    [Op.adjacent]: "-|-",
+    [Op.strictLeft]: "<<",
+    [Op.strictRight]: ">>",
+    [Op.noExtendRight]: "&<",
+    [Op.noExtendLeft]: "&>",
+    [Op.any]: "ANY",
+    [Op.all]: "ALL",
+    [Op.and]: " AND ",
+    [Op.or]: " OR ",
+    [Op.col]: "COL",
+    [Op.placeholder]: "$$PLACEHOLDER$$",
+    [Op.match]: "@@"
+  },
+  OperatorsAliasMap: {},
+  setOperatorsAliases(aliases) {
+    if (!aliases || _.isEmpty(aliases)) {
+      this.OperatorsAliasMap = false;
+    } else {
+      this.OperatorsAliasMap = __spreadValues({}, aliases);
+    }
+  },
+  _replaceAliases(orig) {
+    const obj = {};
+    if (!this.OperatorsAliasMap) {
+      return orig;
+    }
+    Utils.getOperators(orig).forEach((op) => {
+      const item = orig[op];
+      if (_.isPlainObject(item)) {
+        obj[op] = this._replaceAliases(item);
+      } else {
+        obj[op] = item;
+      }
+    });
+    _.forOwn(orig, (item, prop) => {
+      prop = this.OperatorsAliasMap[prop] || prop;
+      if (_.isPlainObject(item)) {
+        item = this._replaceAliases(item);
+      }
+      obj[prop] = item;
+    });
+    return obj;
+  }
 };
-var operators_default = Op;
-module.exports = Op;
+module.exports = OperatorHelpers;
 //# sourceMappingURL=operators.js.map
